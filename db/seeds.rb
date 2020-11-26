30976b59be3d362abbcecbf28ac8d4264426a092
Elizabeth Giraldo <elizabethgiraldopinto@gmail.com>
2020-11-26 12:03:16 +0800

    models and associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+filepath = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+
+ingredients = JSON.parse(File.read(filepath))
